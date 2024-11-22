b57fc1754e7f865de8736f35100d0b0d04b342cb
Kartik deshmukh <kartikdd90@gmail.com>
2024-11-22 14:41:04 +0530

    Entry

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package karmaclips
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+
+	"karmaclips/routes"
+)
+
+func main() {
+	err := godotenv.Load()
+	opts := &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stderr, opts))
+	if err != nil {
+		logger.Error("unable to load .env")
+	}
+
+	app := routes.Routes()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
+	app.Listen(":6969")
+}
